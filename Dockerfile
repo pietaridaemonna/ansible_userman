677dec69c32a6b7319428862ba5326ebe93f39d0
pietaridaemonna <pietari.daemonna@gmail.com>
2017-08-09 10:09:52 +0200

    working file gen

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM centos:7
 MAINTAINER Peter Ducai <peter.ducai@gmail.com>
-ENV NODEJS_VERSION=v4.4.7
+ENV NODEJS_VERSION=v6.11.2
 ENV PATH=/apps/node/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin
 
 RUN yum -y install make gcc gcc-c++ && yum -y clean all
-RUN mkdir /apps && cd /apps && curl -s -L -O https://nodejs.org/dist/${NODEJS_VERSION}/node-${NODEJS_VERSION}-linux-x64.tar.xz && tar xf node-${NODEJS_VERSION}-linux-x64.tar.xz && mv node-${NODEJS_VERSION}-linux-x64 node
+#RUN mkdir /apps && cd /apps && curl -s -L -O https://nodejs.org/dist/${NODEJS_VERSION}/node-${NODEJS_VERSION}-linux-x64.tar.xz && tar xf node-${NODEJS_VERSION}-linux-x64.tar.xz && mv node-${NODEJS_VERSION}-linux-x64 node
+RUN rpm -ivh https://rpm.nodesource.com/pub_6.x/el/7/x86_64/nodejs-6.11.2-1nodesource.el7.centos.x86_64.rpm
 
+CMD "sudo npm install"
 CMD "npm start"
--- routes/create_struct.js
+++ routes/create_struct.js
@@ -9,17 +9,36 @@ const fs = require('fs-extra')
 router.post('/', function (req, res, next) {
 
     req.cookies.proj_path = req.body.proj_path;
-    console.log('--------------CREATING folder public/projects/'+req.body.proj_path);
+    //console.log('--------------CREATING folder public/projects/'+req.body.proj_path+'/vars');
 
     try {
-        const dir = 'public/projects/'+req.cookies.proj_path;
+        const dir = 'public/projects/'+req.cookies.proj_path+'/vars';
         fs.ensureDirSync(dir)
-        console.log('success!')
+        console.log('success!');
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
 
-    res.render('teams', {proj_path: req.cookies.proj_path});
+    try{
+        const servers = 'public/projects/'+req.body.proj_path+'/vars/servers.yaml';
+        const teams = 'public/projects/'+req.body.proj_path+'/vars/teams.yaml';
+        const users = 'public/projects/'+req.body.proj_path+'/vars/users.yaml';
+
+        console.log('creating servers...')
+        fs.outputFileSync(users, '---\nusers:\n  - username: myuser\n    name: some admin\n    team: [samplegroup]\n    ssh_keys:\n      - ssh-rsa FJDLJS:FJ:SDJ:F:FDSFKJDSF\n');
+        fs.outputFileSync(teams, '---\nteams:\n  samplegroup:\n    groups: [somegroup]\n      add_keys_to_user: [somegroup]');
+        fs.outputFileSync(servers, '---\nservers:\n  mysamplehost:\n    name: samplehost\n    teams: [samplegroup]');
+    } catch (err){
+        console.error(err);
+    }
+
+// create files
+// ├── main.yml
+// ├── servers.yml
+// ├── teams.yml
+// └── users.yml
+
+    res.cookie('proj_path',req.cookies.proj_path ).render('create_struct', {proj_path: req.cookies.proj_path});
 });
 
 
