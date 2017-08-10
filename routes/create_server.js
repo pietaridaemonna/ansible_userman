97417fd7c44b79aea8a05c6d7bae348a5204764b
pietaridaemonna <pietari.daemonna@gmail.com>
2017-08-10 16:29:18 +0200

    types

--- routes/create_server.js
+++ routes/create_server.js
@@ -10,13 +10,13 @@ router.post('/', function (req, res, next) {
     //req.session.proj_path = req.body.proj_path;
     console.log(req.session.proj_path+'   CREATE_SERVER ------------>');
     console.log('WRITING STUF::');
-    console.log('\n  ' + req.body.hostname + ':\n    name:' + req.body.aliasname + '\n    teams: []'); // + req.body.teams);
+    console.log('\n  ' + req.body.hostname + ':\n    name: ' + req.body.aliasname + '\n    teams: []'); // + req.body.teams);
 
     if (!req.session.proj_path){
         res.redirect('/');
     } else {
         var cdir = process.cwd();
-        fs.appendFileSync(cdir + '/public/projects/'+req.session.proj_path+'/vars/servers.yaml', '\n  ' + req.body.hostname + ':\n    name:' + req.body.aliasname + '\n    teams: ' + req.body.teams);
+        fs.appendFileSync(cdir + '/public/projects/'+req.session.proj_path+'/vars/servers.yaml', '\n  ' + req.body.hostname + ':\n    name: ' + req.body.aliasname + '\n    teams: ' + req.body.teams);
         res.cookie('proj_path',req.session.proj_path ).render('servers', {proj_path: req.session.proj_path});
     }
 
